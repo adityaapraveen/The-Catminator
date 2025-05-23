bcf65c0643c9abee030d2cceba3e459e7efb058b
adityaapraveen <adityapraveen18@gmail.com>
2025-05-23 12:40:57 +0530

    Render trial2

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/The-Catminator/', // Set base path for GitHub Pages
+  base: '/', // Set base path for GitHub Pages
   server: {
     proxy: {
       '/api': {
